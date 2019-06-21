931efdfd7298043ae4e8ecb05493799bb6a82bcc
= <=>
2019-06-21 15:05:25 +0200

    Add project files.

---
+++ Pattern Recognition/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern_Recognition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
